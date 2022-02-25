62491d1f1ab86705c15d4b3e3f6ffb20c23ef432
Jan Mach <janmach1@microsoft.com>
2022-02-25 18:32:46 +0100

    React rewrite - click counter example

---
+++ src/components/click-counter/constants.ts
@@ -0,0 +1,7 @@
+export const widgetName = "click-counter";
+export const widgetDisplayName = "Click counter";
+export const widgetCategory = "Advanced";
+export const widgetSelector = "click-counter";
+export const widgetRuntimeSelector = "click-counter-runtime";
+export const widgetEditorSelector = "click-counter-editor";
+export const widgetIconClass = "widget-icon widget-icon-component";
