d6e750f434d45d1e3f3cffb9042c9585394b91d5
harunrst <harunheybet@microsoft.com>
2022-03-07 17:14:10 +0300

    add tag service

--- src/services/tagService.ts
+++ src/services/tagService.ts
@@ -3,11 +3,11 @@ import { PageContract } from "../contracts/page";
 import { Page } from "../models/page";
 import { Tag } from "../models/tag";
 import { Utils } from "../utils";
-import { MapiClient } from "./mapiClient";
+import { BackendClient } from "./backendClient";
 
 export class TagService {
-    constructor(private readonly mapiClient: MapiClient) { }
-    
+    constructor(private readonly backendClient: BackendClient) { }
+
     public async getTags(scope?: string, filter?: string): Promise<Page<Tag>> {
         let query = "/tags";
 
@@ -19,7 +19,7 @@ export class TagService {
             query = Utils.addQueryParameter(query, `$filter=(startswith(properties/displayName,'${filter}'))`);
         }
 
-        const pageOfTags = await this.mapiClient.get<PageContract<TagContract>>(query, [MapiClient.getPortalHeader("getTags")]);
+        const pageOfTags = await this.backendClient.get<PageContract<TagContract>>(query, [BackendClient.getPortalHeader("getTags")]);
 
         const page = new Page<Tag>();
         page.value = pageOfTags.value.map(x => new Tag(x));
