1f1aef19750b9ae989c990a3bdcbbec0772817a1
Cristina Malin <malincrist@microsoft.com>
2022-03-08 12:07:40 +0100

    added default styles for try it button

--- src/components/operations/operation-details/operationDetailsHandlers.ts
+++ src/components/operations/operation-details/operationDetailsHandlers.ts
@@ -27,8 +27,48 @@ export class OperationDetailsHandlers implements IWidgetHandler {
                         displayName: "Try it button",
                         plugins: ["typography"],
                         defaults: {
+                            allowedStates: [
+                                "hover",
+                                "focus",
+                                "active",
+                                "disabled"
+                            ],
                             background: {
-                                colorKey: "colors/defaultBg"
+                                colorKey: "colors/tryItBg"
+                            },
+                            typography: {
+                                colorKey: "colors/defaultBg",
+                                fontStyle: "normal",
+                                fontWeight: "700",
+                                fontSize: ".8em",
+                            },
+                            size: {
+                                maxHeight: "30px",
+                                minHeight: "30px",
+                                maxWidth: "60px"
+                            },
+                            border: {
+                                left: { width: "0" },
+                                right: { width: "0" },
+                                top: { width: "0" },
+                                bottom: { width: "0" }
+                            },
+                            padding: {
+                                left: "6",
+                                right: "6",
+                                top: "1",
+                                bottom: "1"
+                            },
+
+                            states: {
+                                hover: {
+                                    background: {
+                                        colorKey: "colors/defaultBg"
+                                    },
+                                    typography: {
+                                        colorKey: "colors/tryItBg",
+                                    }
+                                }
                             }
                         }
                     }
